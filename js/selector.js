fc3f531416356feda748d4b594e86d7d9f387a83
qiuleqiu <ggmme@qq.com>
2020-10-14 18:05:34 +0800

    三角符号定位精调

--- js/selector.js
+++ js/selector.js
@@ -188,7 +188,7 @@
 				var thisVal = $(this).val() || '';
 				var thisText = $(this).find('option[value="'+thisVal+'"]').length ? $(this).find('option[value="'+thisVal+'"]').eq(0).text() : '';
 				$('#q-select-box').remove();
-				var $container = $('<div id="q-select-box" style="z-index:2;position: fixed;min-width:' + width + 'px;"><div class="q-select-input-box" style="margin-top:-' + height + 'px;width:' + width + 'px;"><input type="text" style="width: 100%;display: block;border: 1px solid #ccc;background: #fff;padding-right: 12px;height: ' + height + 'px;box-sizing:border-box;" class="q-select-input"><span class="icon" style="top:' + (height / 2 - 2) + 'px;left:' + (width - 12) + 'px"></span></div></div>');
+				var $container = $('<div id="q-select-box" style="z-index:2;position: fixed;min-width:' + width + 'px;"><div class="q-select-input-box" style="margin-top:-' + height + 'px;width:' + width + 'px;"><input type="text" style="width: 100%;display: block;border: 1px solid #ccc;background: #fff;padding-right: 15px;height: ' + height + 'px;box-sizing:border-box;" class="q-select-input"><span class="icon" style="top:' + (height / 2 - Math.sqrt(2)) + 'px;left:' + (width - 15) + 'px"></span></div></div>');
 				var $list = $('<div class="q-select-list" style="box-shadow:0 0 5px #a7a7a7;max-height:500px;overflow:auto;"></div>');
 				
 				refreshQselectList($(this), $list);
